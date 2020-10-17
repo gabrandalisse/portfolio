70ec32a77fe6c126953563d549272e5b1aa7e52b
Gabriel Andres Brandalisse <gabibrandalisse@gmail.com>
2020-10-17 15:47:12 -0300

    Technologies slider added

--- js/index.js
+++ js/index.js
@@ -7,7 +7,19 @@ tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
 tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
 tl.fromTo("header", { opacity: 0 }, { opacity: 1, duration: 1 });
 tl.fromTo(".textBx", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
+tl.fromTo(".btn", { opacity: 0 }, { opacity: 1, duration: 1 });
 tl.fromTo(".about", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
 tl.fromTo(".technologies", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
 tl.fromTo(".work", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
 tl.fromTo(".contact", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
+
+var swiper = new Swiper(".swiper-container", {
+  slidesPerView: 5,
+  spaceBetween: 50,
+  centeredSlides: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  mousewheel: true,
+});
