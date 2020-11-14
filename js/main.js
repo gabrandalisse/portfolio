26bcd9ecf41b7ceb2594a8ba5d217f305de63b35
Gabriel Andres Brandalisse <gabibrandalisse@gmail.com>
2020-11-14 15:10:00 -0300

    intro deleted

--- js/main.js
+++ js/main.js
@@ -1,16 +1,4 @@
-//* Fancy intro animation
-const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
 
-tl.to(".text", { y: "0%", duration: 1, stagger: 0.25 });
-tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
-tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
-tl.fromTo("header", { opacity: 0 }, { opacity: 1, duration: 1 });
-tl.fromTo(".textBx", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
-tl.fromTo(".btn", { opacity: 0 }, { opacity: 1, duration: 1 });
-tl.fromTo(".about", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
-tl.fromTo(".technologies", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
-tl.fromTo(".work", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
-tl.fromTo(".contact", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
 
 //* Add sticky class on scroll to the nav
 window.addEventListener("scroll", function(){
