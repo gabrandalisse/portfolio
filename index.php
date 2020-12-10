4f5e13067c1ca6407243c20b3b0e75a1739024e1
Gabriel Andres Brandalisse <gabibrandalisse@gmail.com>
2020-12-10 18:33:16 -0300

    Responsive work links fixed Work query limited to 4 READY TO DEPLOY ONLY NEED TO configure htacces minify css and js

--- index.php
+++ index.php
@@ -34,8 +34,9 @@
         <div class="content">
             <div class="contentBx">
                 <h3>// Desarollador Web FullStack</h3>
-                <p>Egresado de una escuela secundaria técnica como Técnico en Electrónica, ahora soy un estudiante avanzado de la carrera Ingeniería en Sistemas de Información en la Universidad Tecnológica Nacional Facultad Regional Córdoba y Desarollador Web con conocimientos en los stack  <b>WAMP</b>, <b>MERN</b>, <b>JAM</b>, entre otros. 
+                <p>Egresado de una escuela secundaria técnica como Técnico en Electrónica, ahora soy un estudiante avanzado de la carrera Ingeniería en Sistemas de Información en la Universidad Tecnológica Nacional Facultad Regional Córdoba la cual me permitió conocer el mundo del desarollo web en el cual comenzé a adentrarme. Actualmente poseo conocimientos en varios stack de tecnologías que me permiten llevar mis ideas, proyectos y trabajos a cabo, algunos como el stack <b>WAMP</b>, <b>MERN</b>, <b>JAM</b>, entre otros. 
                 <br> 
+                Lo que me gusta el del mundo del desarollo web es la cantidad de campos en los que se puede trabajar. Personalmente me gusta crecer tanto como desarollador Back End, en donde me gusta todo lo relacionado al desarollo de la lógica de las aplicaciones, buenas prácticas y seguridad, y como desarollador Front End, dónde considero personalmente, que es lo mas importante debido a que es esencial un buen diseño UX/UI para todos los trabajos que impliquen una interaccion con un usuario inexperto.
                 Me considero una persona organizada y con gran motivación, preparado para trabajar y siempre dispuesto a dar lo mejor de mi en cada proyecto.</p>
             </div>
         </div>
