88ca89df8e0353c186ec8e5adb0c1366e00bed9f
Gabriel Andres Brandalisse <gabibrandalisse@gmail.com>
2020-12-12 18:49:29 -0300

    First admin approach

---
+++ admin/templates/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>GAB Portafolio | Administracion</title>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Font Aweasome -->
+  <script
+      src="https://kit.fontawesome.com/e07ab79f6a.js"
+      crossorigin="anonymous"
+    ></script>
+
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+
+  <!-- overlayScrollbars -->
+  <link rel="stylesheet" href="./css/adminlte.min.css">
+  
+  <!-- Google Font: Source Sans Pro -->
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+</head>
