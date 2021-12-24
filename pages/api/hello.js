5de8f5845c5c7090b87ec565f048fec877e877bf
Gabriel Andres Brandalisse <gabibrandalisse@gmail.com>
2021-12-23 23:10:00 -0300

    json cv added

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import cv from '@db/local/CV.json';
 
 export default (req, res) => {
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
+  res.statusCode = 200;
+  res.json(cv);
 }
